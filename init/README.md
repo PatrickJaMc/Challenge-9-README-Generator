0dcaa832dd306cc4b93bc61a298992afb61148b1
C12 <mcpartlandpatrick@gmail.com>
2023-05-18 16:28:51 -0400

    added all inquierer questions

---
+++ init/README.md
@@ -0,0 +1,42 @@
+# ReadMe Generator
+
+## ![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+Create quick and professional ReadMe's for your projects
+
+## Table Of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribute](#contribute)
+* [Test](#test)
+* [Questions](#questions)
+
+## Installation
+
+node, inquierer@8.2.4
+
+## Usage
+
+
+
+## License
+
+MIT
+
+## Contribute
+
+To contribute please contact me via GitHub or Email
+
+## Test
+
+run on command line using node
+
+## Questions
+For questions please contact me via:
+
+### GitHub: PatJaMc
+### Email: mcpartlandpatrick@gmail.com
--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const generateReadMe = ({title, description, installation, usage, license, contribute, test, questions}) =>
+const generateReadMe = ({title, description, installation, usage, license, contribute, test, github, email}) =>
 `# ${title}
+
+## ![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
 
 ## Description
 
@@ -38,8 +40,10 @@ ${contribute}
 ${test}
 
 ## Questions
+For questions please contact me via:
 
-${questions}
+### GitHub: ${github}
+### Email: ${email}
 `
 
 
@@ -50,7 +54,49 @@ inquirer
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?'
-    }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Give your project a description'
+  },
+  {
+      type: 'input',
+      name: 'installation',
+      message: 'What is needed for installation?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Give brief description on app usage'
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Select app usage license',
+    choices: ['MIT', 'Apache', 'Proprietary']
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'Give contribution instructions'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Give brief explanation on how to test app'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Provide your GitHub User Name'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Provide your email'
+  }
+
   ])
   .then((answers) => {
     const readMe = generateReadMe(answers);
